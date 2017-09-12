97bfc8cdeea3361112cb85e1ee9d07ef22560bcf
oscargolding <30827543+oscargolding@users.noreply.github.com>
2017-09-12 16:00:09 +1000

    Update mandelbrot.c

--- Assignment1/mandelbrot.c
+++ Assignment1/mandelbrot.c
@@ -1,99 +1,105 @@
-#include "mandelbrot.h"
-#include <assert.h>
+// Assignment 1 Mandelbrot
+//
+// Completed by
+//  ... (z0000000)
+//  ... (z0000000)
+//
+// Modified on 2017-??-??
+// Tutor's name (dayHH-lab)
+
+// Add your own #includes here
+
 #include <stdio.h>
+#include <stdlib.h>
+#include "mandelbrot.h"
+#include "pixelColor.h"
 
-#define SIZE 3
+// Add your own #defines here
 
-void testEscapeSteps(void);
+#define BREAK_OUT 4
+
+// Add your own function prototypes here
+static complex complexAdd(complex a, complex b);
+static complex complexMultiply(complex a, complex b);
+static double complexAbsolute(complex c);
 static void pixelCalc (complex c);
 
-int main(void) {
 
+// Draw a single Mandelbrot tile, by calculating and colouring each of
+// the pixels in the tile.
+//
+// Your function "returns" this tile by filling in the `pixels` 2D array
+// that is passed in to your function.
+//
+// The `pixels` array is TILE_SIZE high and TILE_SIZE wide.
+// The bottom left corner of the image is pixels[0][0].
+//
+// `center` is a complex number, representing the center of the tile.
+// `z` is used to calculate the distance between each pixel in the image.
+// Each pixel is 2^(-z) apart from each other.
+void drawMandelbrot(pixel pixels[TILE_SIZE][TILE_SIZE],
+        complex center, int z) {
+    // hint: use this array to store the result from escapeGrid.
+    int grid[TILE_SIZE][TILE_SIZE];
 
-    testEscapeSteps();
-    
+    // TODO: COMPLETE THIS FUNCTION
 
-    return 0;
 }
 
-void testEscapeSteps(void) {
+// Determine the number of steps required to escape the Mandelbrot set,
+// for the given complex number `c`.
+int escapeSteps(complex c) {
     
-    complex a = {0, 0};
-    assert(escapeSteps(a) == MAX_STEPS);
-    
-    complex z = {-0.34, -0.0059};
-    assert(escapeSteps(z) == MAX_STEPS);
-    
-    complex b = {20, 0};
-    assert(escapeSteps(b) == 1);
-    
-    complex d = {3, 0};
-    assert(escapeSteps(d) == 1);
-    
-    complex g = {-3, 0};
-    assert(escapeSteps(g) == 1);
-    
-    complex h = {20, 20};
-    assert(escapeSteps(h) == 1);
-    
-    complex lm = {-0.11, -0.74};
-    assert(escapeSteps(lm) == MAX_STEPS);		
-    //double x = -SIZE;
-    //double y = SIZE;
-    //complex c = {x, y};
-    
-    //printf(" c = %lf\n", c);
-   /* while (y > -SIZE) {
-        x = -SIZE;
-        complex c = {x, y};
-        //printf("c = %lf %lf\n", c.re, c.im);
-        while (x < SIZE) {
-            complex c = {x, y};
-            //printf("c = %lf %lf\n", c.re, c.im);
-            if (escapeSteps(c) < MAX_STEPS) {
-                printf(" ");
-                //printf("Steps = %d\n", escapeSteps(c));
-            } else {
-                printf("*");
-                //printf("Steps = %d\n", escapeSteps(c));
-            }
-            //printf("c = %lf %lf\n", c.re, c.im);
+    int steps = 0;
+    complex start = {0, 0};
 
-            x++;
-           
-        }
-        y--;
-        
-    } */
-    double i = 0;
-    double j = 0;
-    while (i < SIZE) {
-        j = 0;
-        complex c = {i, j};
-        while (j < SIZE) {
-            if (escapeSteps(c) == MAX_STEPS) {
-                printf("*");
-            } else {
-                printf(" ");
-            }   
-            j = j + 0.1;  
-        } 
-        printf("\n");
-        i = i + 0.1;
-    }   
-    
-    /*complex c = {0, 0};
-    double x, y;
-    y = c.im;
-    while (y > c.im - 2) {
-        x = c.re;
-        while (x < c.re + 3) {
-            pixelCalc(c);
-            x++;
-        }
-        printf("\n");
-        y--;
-    }*/
+    // TODO: COMPLETE THIS FUNCTION
+    while (complexAbsolute(start) < BREAK_OUT && steps < MAX_STEPS) {
+        start = complexMultiply(start, start);
+        start = complexAdd(start, c);
+        steps++;
+        //printf("%d\n", steps);
+        //if (steps == MAX_STEPS) {
+        //    return NO_ESCAPE;
+       // }
+    }
+    return steps;
+
+}
+
+// Fill a grid of TILE_SIZE by TILE_SIZE pixels, with the number of
+// steps each pixel took to escape the Mandelbrot set.
+void escapeGrid(int grid[TILE_SIZE][TILE_SIZE],
+        complex center, int z) {
+
+    // TODO: COMPLETE THIS FUNCTION
+
+}
+
+// Add your own functions here.
+// Remember to make them static.
+static complex complexAdd(complex a, complex b){
+    // PUT YOUR CODE HERE;
+    complex value;
+    value.re = a.re + b.re;
+    value.im = a.im + b.im;
+
+    return value;
+}
+static complex complexMultiply(complex a, complex b){
+    // PUT YOUR CODE HERE;
+    complex value = {
+        .re = (a.re * b.re) - (a.im * b.im),
+        .im = (a.im * b.re) + (a.re * b.im)
+    };
+
+    return value;
+}
+static double complexAbsolute(complex c) {
+    // PUT YOUR CODE HERE;
+    double ab = (c.re * c.re + c.im * c.im);
+
+    return ab; // CHANGE THIS TO YOUR RETURN VALUE
 }
 static void pixelCalc (complex c) {
     if (escapeSteps(c) < MAX_STEPS) {
@@ -102,4 +108,3 @@ static void pixelCalc (complex c) {
         printf("*");
     }
 }
-
